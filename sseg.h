469549172e1d31cd3473f43394970baad875b19f
LarryPoppins <62465626+LarryPoppins@users.noreply.github.com>
2020-10-01 21:33:19 +0200

    7-Segment Display Magic
    Pulled some handy 7-Segment Display functions together. good stuff.

---
+++ sseg.h
@@ -0,0 +1,31 @@
+//I can't remember which one of the panels i labeled how so, good luck i guess
+//sets numbers to the first panel
+void setSSEG1One();
+void setSSEG1Two();
+void setSSEG1Three();
+void setSSEG1Four();
+void setSSEG1Five();
+void setSSEG1Six();
+void setSSEG1Seven();
+void setSSEG1Eight();
+void setSSEG1Nine();
+void setSSEG1Zero();
+
+////sets numbers to the second panel
+void setSSEG2One();
+void setSSEG2Two();
+void setSSEG2Three();
+void setSSEG2Four();
+void setSSEG2Five();
+void setSSEG2Six();
+void setSSEG2Seven();
+void setSSEG2Eight();
+void setSSEG2Nine();
+void setSSEG2Zero();
+
+//clear all segments of 1st Panel, 2nd Panel, and clear all Segments
+void clearSSEG1All();
+void clearSSEG2All();
+void clearSSEG12All();
+
+void setSSEGNumber(int num);
